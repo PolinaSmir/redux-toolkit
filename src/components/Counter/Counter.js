c7611ff8f98de1a53768f4e8a45a3be613d2f554
Polina <smivasilisa@gmail.com>
2024-07-02 17:50:56 +0200

    Add mapDispatchToProps object

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -11,11 +11,11 @@ const Counter = (props) => {
       <p>Count: {count}</p>
       <label>
         Set step:
-        <input type="number" value={step} onChange={setStep} />
+        <input type="number" value={step} onChange={({ target: { value } }) => setStep(value)} />
       </label>
       <p>Step: {step}</p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <button onClick={() => increment()}>Increment</button>
+      <button onClick={() => decrement()}>Decrement</button>
     </div>
   );
 };
@@ -27,12 +27,18 @@ function mapStateToProps(state) {
   };
 }
 
-function mapDispatchToProps(dispatch) {
-  return {
-    increment: () => dispatch(increment()),
-    decrement: () => dispatch(decrement()),
-    setStep: ({ target: { value } }) => dispatch(setStep(value)),
-  };
-}
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     increment: () => dispatch(increment()),
+//     decrement: () => dispatch(decrement()),
+//     setStep: ({ target: { value } }) => dispatch(setStep(value)),
+//   };
+// }
+
+const mapDispatchToProps = {
+  increment,
+  decrement,
+  setStep,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
