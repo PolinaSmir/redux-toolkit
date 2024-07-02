a45542022de3af275281a2fb4ebfe938277124db
Polina <smivasilisa@gmail.com>
2024-07-02 17:30:40 +0200

    Add step changing

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
-import { increment, decrement } from "../../store/slices/counterSlice";
+import { increment, decrement, setStep } from "../../store/slices/counterSlice";
 
 const Counter = (props) => {
   const { count, step, dispatch } = props;
@@ -8,6 +8,10 @@ const Counter = (props) => {
   return (
     <div>
       <p>Count: {count}</p>
+      <label>
+        Set step:
+        <input type="number" value={step} onChange={({ target: { value } }) => dispatch(setStep(value))} />
+      </label>
       <p>Step: {step}</p>
       <button onClick={() => dispatch(increment())}>Increment</button>
       <button onClick={() => dispatch(decrement())}>Decrement</button>
--- src/store/slices/counterSlice.js
+++ src/store/slices/counterSlice.js
@@ -15,13 +15,16 @@ const counterSlice = createSlice({
     decrement: (state, action) => {
       state.count -= state.step;
     },
+    setStep: (state, action) => {
+      state.step = Number(action.payload);
+    },
   },
 });
 
 const { reducer, actions } = counterSlice;
 
 // action creators
-const { increment, decrement } = actions;
+const { increment, decrement, setStep } = actions;
 
-export { increment, decrement };
+export { increment, decrement, setStep };
 export default reducer;
