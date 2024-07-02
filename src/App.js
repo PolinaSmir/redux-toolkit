a775782b2cb425bdf0de3823d7895b713b45a996
Polina <smivasilisa@gmail.com>
2024-07-02 19:23:54 +0200

    Add libs for  styles + Header skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Counter from "./components/Counter/Counter";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <Counter />;
+  return (
+    <>
+      <Header />
+      <Counter />
+    </>
+  );
 }
 
 export default App;
