5f446ce0006a2bcbf4112b5a5ff1b9785f2ce1c0
Polina <smivasilisa@gmail.com>
2024-07-04 17:24:03 +0200

    Add theme display for Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
 import styles from "./Header.module.scss";
+import { connect } from "react-redux";
+import cx from "classnames";
+import CONSTANTS from "../../constants";
+import { setTheme } from "../../store/slices/themeSlice";
+const { THEMES } = CONSTANTS;
 
-const Header = () => {
+const Header = ({ theme, language, setTheme }) => {
+  const className = cx(styles.header, {
+    [styles.darkTheme]: theme === THEMES.DARK,
+    [styles.lightTheme]: theme === THEMES.LIGHT,
+  });
+
   return (
-    <header className={styles.header}>
+    <header className={className}>
       <h1>My Counter</h1>
       <nav>
         <ul className={styles.navList}>
@@ -14,10 +24,21 @@ const Header = () => {
       </nav>
 
       <div>
-        <button>Switch Theme</button>
+        <button onClick={() => setTheme()}>Switch Theme</button>
       </div>
     </header>
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    theme: state.theme,
+    language: state.lang,
+  };
+};
+
+const mapDispatchToProps = {
+  setTheme,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
